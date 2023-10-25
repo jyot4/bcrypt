7d9c73347576e44b319b2d42e15fe6ab98215084
jyot4 <jyotivyass2000@gmail.com>
2023-10-25 11:26:47 +0530

    use jwt

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ import cors from 'cors'
 import connection from './Connection.js'
 import detail from './Model.js'
 import bcrypt from 'bcrypt'
+import  Jwt  from 'jsonwebtoken'
+import {config} from 'dotenv'
 
 
+config()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use(cors({origin:'http://localhost:3001'}))
+app.use(cors({origin:'http://localhost:3000'}))
+
+const jwtKey = process.env.SCERCT_KEY;
 
 ///.....................Resigter.....................//
 app.post('/formdata' , async(req,res)=>{
@@ -17,6 +22,8 @@ app.post('/formdata' , async(req,res)=>{
 
 
     const {name,email,username,password} = req.body
+
+//...................................hash password.....................
 
 const hashPassword = await bcrypt.hash(password , 10 )
 
@@ -41,11 +48,16 @@ app.post('/login', async(req ,res)=>{
     const  {username , password} = req.body
 
     const findUser = await detail.findOne({username})
-
+//........................................bcrypt compare.....................///
     if(detail){
 
         if(findUser && bcrypt.compareSync(password,findUser.password)){
-            res.status(200).send(findUser.email)
+
+const token = Jwt.sign({userName: username},jwtKey)
+console.log(token)
+
+const SavedToken = ("key" , token)
+            res.status(200).send(SavedToken)
         }
         else{ 
             res.status(400).send("not able to login")
